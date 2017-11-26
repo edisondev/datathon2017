e76f04a92ce4d4e66be943f8214f3a797da64777
Edison Developers <nikolai.kummer@gmail.com>
2017-11-25 20:27:24 -0700

    Add functions
    Run LDA_ckc_data.R

--- LDA_ckc_data.R
+++ LDA_ckc_data.R
@@ -1,6 +1,6 @@
 
 library(tm)
-setwd('C:\\Users\\Nick\\Dropbox\\Work\\Data Science\\13 - Data for Good Datathon\\scripts')
+setwd('C:\\Users\\Nick\\Dropbox\\Work\\Data Science\\13 - Data for Good Datathon\\scripts\\datathon2017')
 
 source("LDA.analysis.R")
 source("topicmodels_json_ldavis.R")
@@ -33,70 +33,35 @@ dfnew=paste(dfn$WhyWeExist,
                    dfn$HowWeDoIT,
                    sep=" ")
 
-docs <- Corpus(VectorSource(dfnew))  
-docs <- tm_map(docs, removePunctuation) 
-docs <- tm_map(docs, removeNumbers)
-docs <- tm_map(docs, tolower)
-docs <- tm_map(docs, removeWords, stopwords("english"))
-docs <- tm_map(docs, removeWords, stopwords("french"))
-docs <- tm_map(docs, stemDocument) #remove endings
-#docs <- tm_map(docs, removeWords, c("program","programs","services","service","provide", "community","support","calgary","alberta","canadian","canada","edmonton"))
-dtm <- DocumentTermMatrix(docs) 
-
-#remove duplicate documents:
-ui = unique(dtm$i)
-dtm.new = dtm[ui,]
+lda_num_topics=15
+lda.mod =LDA.analysis(text_vector = dfnew,num_topics = lda_num_topics)
 
 
+#Get term weights
+require(data.table)
+#library(wordcloud)
+library(ggplot2)
+library(ggrepel)
 
-# #Caculate optimal number of topics: http://davidmeza1.github.io/2015/07/20/topic-modeling-in-R.html
-library(Rmpfr)
-term_tfidf <- tapply(dtm$v/slam::row_sums(dtm)[dtm$i], dtm$j, mean) *log2(tm::nDocs(dtm)/slam::col_sums(dtm > 0))
-summary(term_tfidf)
-# 
-#remove words
-llisreduced.dtm <- dtm[,term_tfidf >= 0.005]
-summary(slam::col_sums(llisreduced.dtm))
-# 
-# harmonicMean <- function(logLikelihoods, precision = 2000L) {
-#   llMed <- median(logLikelihoods)
-#   as.double(llMed - log(mean(exp(-mpfr(logLikelihoods,
-#                                        prec = precision) + llMed))))
-# }
-# seqk <- seq(2, 100, 1)
-# burnin <- 1000
-# iter <- 1000
-# keep <- 50
-# 
-# system.time(fitted_many <- lapply(seqk, function(k) topicmodels::LDA(llisreduced.dtm, k = k,
-#                                                                      method = "Gibbs",control = list(burnin = burnin,
-#                                                                                                      iter = iter, keep = keep) )))
-# logLiks_many <- lapply(fitted_many, function(L)  L@logLiks[-c(1:(burnin/keep))])
-# hm_many <- sapply(logLiks_many, function(h) harmonicMean(h))
-# optimal_topic_number <-  seqk[which.max(hm_many)]  #- optimal topics number
-optimal_topic_number <-15
-system.time(llis.model <- topicmodels::LDA(llisreduced.dtm, optimal_topic_number, method = "Gibbs", control = list(iter=2000, seed = 0622)))
-
-
-#Create labels for topics  - join 3 most common words
-library(tidyr)
-llis.topics <- topicmodels::topics(llis.model, 1)
-llis.terms <- as.data.frame(topicmodels::terms(llis.model, 30), stringsAsFactors = FALSE)
-
-topicTerms <- tidyr::gather(llis.terms, Topic)
-topicTerms <- cbind(topicTerms, Rank = rep(1:30))
-topTerms <- dplyr::filter(topicTerms, Rank < 5)
-topTerms <- dplyr::mutate(topTerms, Topic = stringr::word(Topic, 2))
-topTerms$Topic <- as.numeric(topTerms$Topic)
-topicLabel <- data.frame()
-for (i in 1:optimal_topic_number){
-  z <- dplyr::filter(topTerms, Topic == i)
-  l <- as.data.frame(paste(z[1,2], z[2,2], z[3,2],z[4,2], sep = " " ), stringsAsFactors = FALSE)
-  topicLabel <- rbind(topicLabel, l)
+term_weights=topicmodels::posterior(lda.mod$lda_model)$terms
+term_weight=data.table(term_weights)
+for (i in 1:dim(n)[1]) {
+  words=return_list$topicTerms[,i]
+  weights=unname(sort(term_weight[i,],decreasing=TRUE)[1,1:30])
+  dfwc=data.frame(words,t(weights))
+  png(filename = paste(i, ".png", sep=""))
+  wordcloud(dfwc$words, 
+            dfwc$t.weights.,
+            random.order=FALSE,
+            colors=brewer.pal(8, "Dark2"))
+  dev.off()
 }
 
-colnames(topicLabel) <- c("Label")
-topicLabel
+
+
+
+
+
 
 
 
